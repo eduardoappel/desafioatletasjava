8120893b24b4fac6ad0aecce2f6ec47594c4d142
eduardoappel <eduardo.appel@gmail.com>
2025-03-25 15:05:42 -0300

    Alterado Application add loop dados atletas

--- src/application/Appilcation.java
+++ src/application/Appilcation.java
@@ -23,6 +23,59 @@ public class Appilcation {
         
         System.out.print("Qual a quantidade de Atletas: ");
         int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.println("Digite os dados do atleta numero " + i +":"); 
+
+            System.out.print("Digite o nome do Atleta: ");
+            sc.nextLine();
+            String nome = sc.nextLine();
+
+            System.out.print("Digite o sexo: ");
+            double sexo = sc.nextLine().charAt(0);
+            while( sexo != 'M' && sexo != 'F'){
+                System.out.print("Valor invalido! Favor digitar M ou F: ");
+                sexo = sc.nextLine().charAt(0);
+            }
+
+            System.out.print("Digite a altura: ");
+            double altura = sc.nextDouble();
+            while( altura <= 0){
+                System.out.print("Valor invalido! Favor digitar um valor positivo: ");
+                altura = sc.nextDouble();
+            }
+            System.out.print("Digite o peso: ");
+            double peso = sc.nextDouble();                       
+            while( peso <= 0){
+                System.out.printf("Valor invalido! Favor digitar um valor positivo: ");
+                peso = sc.nextDouble();
+            }
+
+            if (altura > maiorAltura) {
+                maiorAltura = altura;
+                nomeMaisAlto = nome;
+            }
+            somaAltura += altura;
+
+            if (sexo == 'M') {
+                totalHomens++;
+            } else {
+                totalMulheres++;
+                somaAlturaMulheres += altura;
+            }
+
+            pesoMedio += peso;
+
+            porcentagemHomens = (totalHomens * 100) / n;
+         
+            if (totalMulheres == 0) {
+                alturaMediaMulheres = 0;
+            } else {
+                alturaMediaMulheres = somaAlturaMulheres / totalMulheres;
+            }
+
+        }        
     }
 
 }
